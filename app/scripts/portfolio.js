bb27494f55ebb6327e86ed76f58a9f8153225bdf
Joshua Briley <josh@thebrileys.com>
2015-11-23 08:46:31 -0500

    Update portfolio.

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -5,6 +5,7 @@ var pItems = new Firebase('https://jkb-portfolio.firebaseio.com/portfolioItems')
 // Retrieve new posts as they are added to our database
 pItems.on('child_added', function(snapshot) {
   var newItem = snapshot.val();
-  console.log('Name: ' + newItem.name);
-  console.log('Url: ' + newItem.url);
+  $('<h1><a href="' + newItem.url + '" target="_blank">' + newItem.name + '</a></h1>').appendTo('.portfolio-items');
+  $('<p>' + newItem.description + '</p>').appendTo('.portfolio-items');
+  $('<p><a href="' + newItem.codeUrl + '" target="_blank">View Code</a></p>').appendTo('.portfolio-items');
 });
